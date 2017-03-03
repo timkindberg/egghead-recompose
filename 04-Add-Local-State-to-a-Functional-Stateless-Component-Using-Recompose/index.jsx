3b6a41c393e67ecc37ce30ecf819c3d0a920016f
Tim Kindberg <tim.kindberg@kroger.com>
2017-03-02 22:58:38 -0500

    Lesson 4

--- 04-Add-Local-State-to-a-Functional-Stateless-Component-Using-Recompose/index.jsx
+++ 04-Add-Local-State-to-a-Functional-Stateless-Component-Using-Recompose/index.jsx
@@ -7,18 +7,17 @@ Learn how to use the 'withState' and 'withHandlers' higher order
 components to easily add local state to your functional stateless
 components. No need for classes!
 */
-
 const { Component } = React;
 const { compose, withState, withHandlers } = Recompose;
 
-const withToggling = compose(
+const withToggle = compose(
   withState('toggledOn', 'toggle', false),
   withHandlers({
     show: ({ toggle }) => (e) => toggle(true),
     hide: ({ toggle }) => (e) => toggle(false),
     toggle: ({ toggle }) => (e) => toggle((current) => !current)
   })
-);
+)
 
 const StatusList = () =>
   <div className="StatusList">
@@ -27,21 +26,21 @@ const StatusList = () =>
     <div>active</div>
   </div>;
 
-const Status = withToggling(({ status, toggle, toggledOn }) =>
+const Status = withToggle(({ status, toggledOn, toggle }) =>
   <span onClick={ toggle }>
     { status }
     { toggledOn && <StatusList /> }
   </span>
 );
 
-const Tooltip = withToggling(({ text, children, show, hide, toggledOn }) =>
+const Tooltip = withToggle(({ text, children, toggledOn, show, hide }) =>
   <span>
     { toggledOn && <div className="Tooltip">{ text }</div> }
     <span onMouseEnter={ show } onMouseLeave={ hide }>{ children }</span>
   </span>
 );
 
-const User = ({ name, status, dispatch }) =>
+const User = ({ name, status }) =>
   <div className="User">
     <Tooltip text="Cool Dude!">{ name }</Tooltip>—
     <Status status={ status } />
