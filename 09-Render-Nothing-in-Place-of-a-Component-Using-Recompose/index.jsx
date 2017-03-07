8365416e57a5981f7d4d38a203f63ae99a1fa5b4
Tim Kindberg <tim.kindberg@kroger.com>
2017-03-06 20:08:28 -0500

    Lesson 9

--- 09-Render-Nothing-in-Place-of-a-Component-Using-Recompose/index.jsx
+++ 09-Render-Nothing-in-Place-of-a-Component-Using-Recompose/index.jsx
@@ -3,13 +3,12 @@ TITLE:
 Render Nothing in Place of a Component using Recompose
 
 DESCRIPTION:
-Learn how to use the ‘branch’ and ‘renderNothing’
-higher-order components to render nothing when a
-certain prop condition is met. Sometimes you only
-want to render a component when valid props exist
-or are in a certain condition; ‘renderNothing’ is
-an easy way to completely remove the component when
-you don’t need to show it.
+Learn how to use the ‘branch’ and ‘renderNothing’ higher-order
+components to render nothing when a certain prop condition is
+met. Sometimes you only want to render a component when valid
+props exist or are in a certain condition; ‘renderNothing’ is
+an easy way to completely remove the component when you don’t
+need to show it.
 */
 const { Component } = React;
 const { compose, branch, renderNothing } = Recompose;
@@ -17,13 +16,10 @@ const { compose, branch, renderNothing } = Recompose;
 const User = ({ name, status }) =>
   <div className="User">{ name }—{ status }</div>;
 
-const userIsNotActive = ({ user }) => user.status !== 'active';
+const userIsNotActive = ({ status }) => status !== 'active';
+const hideIfNotActive = branch(userIsNotActive, renderNothing);
 
-const enhance = compose(
-  branch(userIsNotActive, renderNothing)
-);
-
-const FeaturedUser = enhance(({ user: { name, status } }) =>
+const FeaturedUser = hideIfNotActive(({ name, status }) =>
   <div>
     <h3>Today's Featured User</h3>
     <User name={ name } status={ status } />
@@ -44,13 +40,13 @@ const users = [
   { name: "Jim", status: "pending" },
 ];
 
-const featuredUser = users[0];
+const featured = users[getRandomInt(0, 3)];
 
 const App = () =>
   <div className="App">
     <h2>User Management</h2>
     <hr />
-    <FeaturedUser user={ featuredUser } />
+    <FeaturedUser name={ featured.name } status={ featured.status } />
     <UserList users={ users } />
   </div>;
 
@@ -58,3 +54,9 @@ ReactDOM.render(
   <App />,
   document.getElementById('main')
 );
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
