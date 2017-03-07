44412e2699bae13c8b6145e2a25d17eaab3eee95
Tim Kindberg <tim.kindberg@kroger.com>
2017-03-06 22:22:35 -0500

    Lesson 11

--- 11-Lock-Props-Using-Recompose/index.jsx
+++ 11-Lock-Props-Using-Recompose/index.jsx
@@ -9,16 +9,14 @@ unable to be overridden.
 const { Component } = React;
 const { withProps } = Recompose;
 
-// const HomeLink = ({ children }) => <a href="/">{ children }</a>;
-
-const HomeLink = withProps({ href: '/' })('a');
-const ProductsLink = withProps({ href: '/products' })('a');
-const CheckoutLink = withProps({ href: '/checkout' })('a');
+const HomeLink = withProps(({ query }) => ({ href: '#/?query=' + query }))('a');
+const ProductsLink = withProps({ href: '#/products' })('a');
+const CheckoutLink = withProps({ href: '#/checkout' })('a');
 
 const App = () =>
   <div className="App">
     <header>
-      <HomeLink>MY LOGO</HomeLink>
+      <HomeLink query="logo">Logo</HomeLink>
     </header>
     <nav>
       <HomeLink>Home</HomeLink>
